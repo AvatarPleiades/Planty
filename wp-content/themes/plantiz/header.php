2d97d6b0eb1fe49396cd53e847e6a684a8fdb2a5
AvatarPleiades <berry.julien57@gmail.com>
2023-05-16 09:22:13 +0200

    Ajout toggle burger + modif pages mentions legales

--- wp-content/themes/plantiz/header.php
+++ wp-content/themes/plantiz/header.php
@@ -3,21 +3,37 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+<!-- font-family -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-        <header class="site-header sticky-top directionRow">
-            <a href="<?php get_stylesheet_directory_uri(); ?> /plantiiz/index.php">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Logo_miniature.png" alt="Logo Planty" id="logo">
-            </a>
-            <nav class="navbar">
-                    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-                    <li><a title="Admin" href="http://localhost/plantiiz/wp-admin/">Admin</a></li>
-                    <button onclick="window.location.href= 'plantiiz/commander/';" class="button-commander"> Commander </button>	
-            </nav>
-        </header>
+<body>
+    <nav class="navbar navbar-expand-lg bg-white fixed-top">
+		<div class="container-fluid">
+			<a href="<?php get_stylesheet_directory_uri(); ?> /plantiiz/index.php"> 
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Logo_miniature.png" alt="Logo Planty" id="logo"> </a>
+		  	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+		 	</button>
+		  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			  	<?php 
+					wp_nav_menu([
+					'theme_location' => 'main-menu',
+					'container'=>false,
+					'menu_class' => 'navbar-nav ms-auto mb-2 mb-lg-0'
+					])
+				?>
+				<button onclick="window.location.href= 'planty_wp/commander/';" class="button-commander"> Commander </button>	
+		 	</div>
+		</div>
+	</nav>
             
 <div id="container">
 <main id="content" role="main">
